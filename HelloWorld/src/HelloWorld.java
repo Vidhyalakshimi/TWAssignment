39aba98953e2b0207d4d8fd2160406c97058dde8
Vidhyalakshimi <svidhyalakshimi@gmail.com>
2016-07-07 10:55:39 +0530

    Adding first excercise HelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Vidhya on 07-07-2016.
+ */
+
+import java.io.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}
