148eed0ae88cad6e8eca98eb73060fc29e578eaa
Vidhyalakshimi <svidhyalakshimi@gmail.com>
2016-07-17 18:49:31 +0530

    Added FizzBuzz

---
+++ FizzBuzzClass.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import java.util.*;
+
+public class FizzBuzzClass {
+
+    public static void FizzBuzz() {
+        int i;
+       for (i = 1; i <= 100; ++i ) {
+           if(i % 3 == 0)
+               System.out.print("Fizz");
+           if(i % 5 == 0)
+               System.out.print("Buzz");
+           if(i % 5 != 0 && i%3 != 0 )
+               System.out.print(i);
+            System.out.println();
+       }
+
+    }
+     public static void main(String[] args){
+        FizzBuzz();
+    }
+}
+
--- HorizontalLine/src/HorizontalLine.java
+++ HorizontalLine/src/HorizontalLine.java
@@ -6,26 +6,21 @@ import java.io.*;
 import java.util.*;
 
 public class HorizontalLine {
+    public static void FizzBuzz() {
+        int i;
+        for (i = 1; i <= 100; ++i ) {
+            if(i % 3 == 0)
+                System.out.print("Fizz");
+            if(i % 5 == 0)
+                System.out.print("Buzz");
+            if(i % 5 != 0 && i%3 != 0 )
+                System.out.print(i);
+            System.out.println();
+        }
+
+    }
     public static void main(String[] args){
-        int n,i,j,k;
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter n:");
-        n = s.nextInt();
-        for(i = n, k = 0; i > 0 && k < (n*n-n)/2; --i,k= k + 2) {
-            for(j = 1; j < i; ++j)
-                System.out.print(" ");
-            for(j = 0; j <= k; ++j)
-                System.out.print("*");
-            System.out.println();
-        }
-        System.out.println("Vidhya");
-        for(i = 1, k = (n*n-n)/2 - 1; i <= n && k >= 0 ; ++i,k= k - 2) {
-            for(j = 1; j <= i; ++j)
-                System.out.print(" ");
-            for(j = 0; j <= k; ++j)
-                System.out.print("*");
-            System.out.println();
-        }
+        FizzBuzz();
     }
 }
 
